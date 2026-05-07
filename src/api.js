1ce0e6a8050ee2674389b9786161d39569f1fa2e
navadeepa241-cloud <navadeepa241@gnail.com>
2026-05-07 14:24:40 +0530

    initial commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
   
 const API = axios.create({ 
-  baseURL: 'https://user-manager-with-auth.onrender.com/api/users' 
+  baseURL: `${import.meta.env.VITE_API_URL}/api/users' 
 }); 
   
 API.interceptors.request.use((req) => { 
