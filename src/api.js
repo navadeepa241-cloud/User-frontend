a436e22641de207f0c8eac4bc54205b4330f4d5e
navadeepa241-cloud <navadeepa241@gnail.com>
2026-05-07 14:39:29 +0530

    fixed api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
   
 const API = axios.create({ 
-  baseURL: `${import.meta.env.VITE_API_URL}/api/users' 
+  baseURL: `${import.meta.env.VITE_API_URL}/api/users` 
 }); 
   
 API.interceptors.request.use((req) => { 
